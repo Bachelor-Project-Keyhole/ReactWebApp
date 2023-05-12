cd9caddff03aa3a8525805cd27f47f104194b643
IliyaNikov <74715206+IliyaNikov@users.noreply.github.com>
2023-05-12 13:58:02 +0200

    feat: ManageOrganization page
    Implemented component for UserCard, added it to the CardList compnent
    and implemented new screen for ManageOrganization

--- client/app.tsx
+++ client/app.tsx
@@ -4,8 +4,12 @@ import { WeatherProvider } from './src/contexts/WeatherContext/WeatherContext'
 import Home from './src/screens/Home'
 import Menu from './src/screens/Menu'
 import ManageDatapoints from './src/screens/ManageDatapoints/ManageDatapoints'
+import ManageUsers from './src/screens/ManageOrganization/ManageOrganization'
 
 function App (): JSX.Element {
+
+    const users = [{name: 'Tamas', email: 'Tamas@Tamas.com', role: 'admin', status: 'accepted'},
+        {name: 'Ilia', email: 'Ilia@Tamas.com', role: 'mod', status: 'pending'}]
   return (
     <WeatherProvider>
         <nav>
@@ -16,8 +20,11 @@ function App (): JSX.Element {
                 <li>
                     <Link to="/menu">Menu</Link>
                 </li>
-                 <li>
+                <li>
                     <Link to="/manage-datapoint">Manage Datapoint</Link>
+                </li>
+                <li>
+                    <Link to="/manage-organization">Manage Organization</Link>
                 </li>
             </ul>
         </nav>
@@ -25,6 +32,7 @@ function App (): JSX.Element {
             <Route path="/" element={<Home/>}/>
             <Route path="/menu" element={<Menu/>}/>
             <Route path="/manage-datapoint" element={<ManageDatapoints/>}/>
+            <Route path="/manage-organization" element={<ManageUsers companyName='KeyHole' users={users} />}/>
         </Routes>
         {/* <Home/> */}
         {/* <Menu/> */}
