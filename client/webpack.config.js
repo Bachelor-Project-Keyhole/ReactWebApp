93ab2b805332a6f46ad35fe92c854cef71f77d1d
IliyaNikov <74715206+IliyaNikov@users.noreply.github.com>
2023-05-30 10:47:11 +0200

    fix: public path depending on environment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
 module.exports = {
   mode: 'development',
   entry: './main.tsx',
   devtool: 'inline-source-map',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
-    // publicPath: '/'
+    filename: 'bundle.js',
+    publicPath: ASSET_PATH
   },
   devServer: {
     static: './dist',
